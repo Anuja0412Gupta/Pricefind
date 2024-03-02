8a48089d81bade2afd93360a9b0febaa69cd68e9
Anuja0412Gupta <161848125+Anuja0412Gupta@users.noreply.github.com>
2024-03-02 11:27:02 +0530

    Update new.py

--- new.py
+++ new.py
@@ -7,12 +7,11 @@ headers = {'Accept': '*/*',
 headers1 = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'Accept-Encoding': 'gzip, deflate, br',
     'Accept-Language': 'en-US,en;q=0.9',
-    'Referer': 'https://www.flipkart.com/',
+    'Accept-Encoding': 'gzip, deflate, br',
     'Connection': 'keep-alive',
-    'Upgrade-Insecure-Requests': '1',
 }
+
 flipkart_url=''
 amazon_url=''
 def flipkart(name):
